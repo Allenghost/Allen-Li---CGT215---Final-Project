d9a26d135845c4c22cc3473bf52b4a5caf86c08a
Allenghost <allen0901.tw@gmail.com>
2024-04-17 00:50:48 -0400

    checkpoint

--- Allen Li - CGT215 - Final Project.cpp
+++ Allen Li - CGT215 - Final Project.cpp
@@ -2,11 +2,210 @@
 //
 
 #include <iostream>
+#include <SFML/Graphics.hpp>
+#include <math.h>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace sf;
+using namespace std;
+
+void printMenu() {
+    cout << "Please Select which operation to perform:" << endl;
+    cout << "1. Red Image" << endl;
+    cout << "2. Green Image" << endl;
+    cout << "3. Blue Image" << endl;
+    cout << "4. Gray Scale" << endl;
+    cout << "5. Blur Effect" << endl;
+    cout << "6. Sharpen Effect" << endl;
+    cout << "7. Exit" << endl;
+    cout << "Your Selection: ";
 }
+
+void Red(Image input, Image output, int width, int height) {
+    for (int y = 0; y < height - 1; y++) {
+        for (int x = 0; x < width - 1; x++) {
+            Color c = input.getPixel(x, y);
+            Color rC(c.r, 0, 0);
+            output.setPixel(x, y, rC);
+        }
+    }
+
+    RenderWindow window(VideoMode(width, height), "Here's the output");
+    Sprite sprite1;
+    Texture tex1;
+    tex1.loadFromImage(output);
+    sprite1.setTexture(tex1);
+    window.clear();
+    window.draw(sprite1);
+    window.display();
+    while (true);
+}
+
+void Green(Image input, Image output, int width, int height) {
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            Color c = input.getPixel(x, y);
+            Color gC(0, c.g, 0);
+            output.setPixel(x, y, gC);
+        }
+    }
+
+    RenderWindow window(VideoMode(width, height), "Here's the output");
+    Sprite sprite1;
+    Texture tex1;
+    tex1.loadFromImage(output);
+    sprite1.setTexture(tex1);
+    window.clear();
+    window.draw(sprite1);
+    window.display();
+    while (true);
+}
+
+void Blue(Image input, Image output, int width, int height) {
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            Color c = input.getPixel(x, y);
+            Color bC(0, 0, c.b);
+            output.setPixel(x, y, bC);
+        }
+    }
+
+    RenderWindow window(VideoMode(width, height), "Here's the output");
+    Sprite sprite1;
+    Texture tex1;
+    tex1.loadFromImage(output);
+    sprite1.setTexture(tex1);
+    window.clear();
+    window.draw(sprite1);
+    window.display();
+    while (true);
+}
+
+void Gray(Image input, Image output, int width, int height) {
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            Color c = input.getPixel(x, y);
+            int average = (c.r + c.g + c.b) / 3;
+            Color grayScale(average, average, average);
+            output.setPixel(x, y, grayScale);
+        }
+    }
+
+    RenderWindow window(VideoMode(width, height), "Here's the output");
+    Sprite sprite1;
+    Texture tex1;
+    tex1.loadFromImage(output);
+    sprite1.setTexture(tex1);
+    window.clear();
+    window.draw(sprite1);
+    window.display();
+    while (true);
+}
+
+void Blur(Image input, Image output, int width, int height) {
+    for (int y = 0; y < height - 1; y++) {
+        for (int x = 0; x < width - 1; x++) {
+            int minX = max(x - 1, 0);
+            int maxX = min(x + 1, width - 1);
+            int minY = max(y - 1, 0);
+            int maxY = min(y + 1, height - 1);
+            int pixCount = 0;
+            int rCount = 0;
+            int bCount = 0;
+            int gCount = 0;
+            for (int pixY = minY; pixY <= maxY; pixY++) {
+                for (int pixX = minX; pixX <= maxX; pixX++) {
+                    pixCount++;
+                    Color pixC = input.getPixel(pixX, pixY);
+                    rCount += pixC.r;
+                    gCount += pixC.g;
+                    bCount += pixC.b;
+                }
+            }
+            Color blurC = Color(rCount / pixCount, gCount / pixCount, bCount / pixCount);
+            output.setPixel(x, y, blurC);
+        }
+    }
+    Texture tex1;
+    tex1.loadFromImage(output);
+    RenderWindow window(VideoMode(width, height), "Here's the output");
+    Sprite sprite1;
+    sprite1.setTexture(tex1);
+    window.draw(sprite1);
+    window.display();
+
+    while (true);
+}
+
+void Sharpen(Image input, Image output, int width, int height) {
+    for (int y = 0; y < height - 1; y++) {
+        for (int x = 0; x < width - 1; x++) {
+            int minX = max(x - 1, 0);
+            int maxX = min(x + 1, width - 1);
+            int minY = max(y - 1, 0);
+            int maxY = min(y + 1, height - 1);
+            int rCount = 0;
+            int bCount = 0;
+            int gCount = 0;
+            for (int pixY = minY; pixY <= maxY; pixY++) {
+                for (int pixX = minX; pixX <= maxX; pixX++) {
+                    Color pixC = input.getPixel(pixX, pixY);
+                    rCount += pixC.r;
+                    gCount += pixC.g;
+                    bCount += pixC.b;
+                }
+            }
+            Color sharpC = Color(2* pixC.r - blurC.r, 2 * pixC.g - blurC.g, 2 * pixC.b - blurC.b);
+            output.setPixel(x, y, blurC);
+        }
+    }
+    Texture tex1;
+    tex1.loadFromImage(output);
+    RenderWindow window(VideoMode(width, height), "Here's the output");
+    Sprite sprite1;
+    sprite1.setTexture(tex1);
+    window.draw(sprite1);
+    window.display();
+
+    while (true);
+}
+
+int main() {
+    int choice;
+    Image img;
+    img.loadFromFile("images1/cat.png");
+    int width = img.getSize().x;
+    int height = img.getSize().y;
+    Image outImg;
+    outImg.create(width, height);
+
+    do {
+        printMenu();
+        std::cin >> choice;
+        if (choice > 6 || choice < 1) {
+            return 0;
+        }
+        else if (choice == 1) {
+            Red(img, outImg, width, height);
+        }
+        else if (choice == 2) {
+            Green(img, outImg, width, height);
+        }
+        else if (choice == 3) {
+            Blue(img, outImg, width, height);
+        }
+        else if (choice == 4) {
+            Gray(img, outImg, width, height);
+        }
+        else if (choice == 5) {
+            Blur(img, outImg, width, height);
+        }
+        else if (choice == 6) {
+            Sharpen(img, outImg, width, height);
+        }
+        
+    } while (true);
+}
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
